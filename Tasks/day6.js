66e73cc47dde3e29e67dd64c7af5214d2797a3fa
goku-code <gedu409@gmail.com>
2024-08-01 22:50:18 +0530

    updated

--- Tasks/day6.js
+++ Tasks/day6.js
@@ -1,10 +1,8 @@
 fetch("https://restcountries.com/v3.1/all")
   .then(response => response.json())
   .then(data => {
-    // Filter Asian countries (using either continent or region)
     const asianCountries = data.filter(country => country.continent === "Asia" || country.region === "Asia");
 
-    // Print details by accessing specific properties
     console.log("Asian Countries");
     asianCountries.forEach(country => {
       console.log(`Name: ${country.name.common}, Capital: ${country.capital}`);
@@ -23,12 +21,11 @@ fetch("https://restcountries.com/v3.1/all")
       console.log(country.name,country.capital,country.flag);
     });
 
- // Calculate total population using reduce
  console.log("Total population");
  const totalPopulation = data.reduce((accumulator, country) => accumulator + country.population, 0);
  console.log("\nTotal Population:", totalPopulation);
 
- // Find countries using US dollars as currency
+
  console.log("\nCountries using US Dollars (USD):");
 
 const countriesUsingUSD = data.filter(country => {
